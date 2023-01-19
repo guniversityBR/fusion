0439d9b4bc15bdc26e50ea6be28686fd9feab5b1
Geek University <contato@geekuniversity.com.br>
2023-01-19 12:13:18 +0000

    Enviando arquivos para deploy no Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fusion.wsgi --log-file -
---
+++ fusion/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for fusion project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fusion.settings')
+
+application = Cling(MediaCling(get_wsgi_application()))
+
